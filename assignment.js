61b3ccc68fa182d08fa292f5bf30a76d9a4b337b
enaye <naserabunemeh@gmail.com>
2023-04-30 01:42:16 -0500

    Added assignment implementation
    Sets certain attributes to the assignments such as it having a title, description, and a due date.

---
+++ assignment.js
@@ -0,0 +1,19 @@
+class Assignment {
+  constructor(title, description, dueDate) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  getDescription() {
+    return this.description;
+  }
+
+  getDueDate() {
+    return this.dueDate;
+  }
+}
